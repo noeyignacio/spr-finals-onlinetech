e1bb98800d790c6d1969349e39653bdc51ee0caa
Francis Ignacio <48373989+noeyignacio@users.noreply.github.com>
2020-03-16 13:35:40 +0800

    Backend, First Commit

---
+++ backend/src/test/java/finalproject/onlinetech/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.onlinetech.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
